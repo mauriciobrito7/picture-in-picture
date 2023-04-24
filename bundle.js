ff4dc1303bbaa728447dd2fe21524a9dbe76298e
Mauricio Brito <maubot01@gmail.com>
2023-04-24 17:56:44 -0300

    feat: built added

---
+++ bundle.js
@@ -0,0 +1,26 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+
+const video = document.querySelector('#video');
+const button = document.querySelector('#button');
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    video.srcObject = mediaStream;
+    video.onloadedmetadata = () => {
+      video.play();
+    };
+  } catch (error) {
+    console.error('Whoops, something went wrong:', error);
+  }
+}
+button.addEventListener('click', async () => {
+  button.disabled = true;
+  await video.requestPictureInPicture();
+  button.disabled = false;
+});
+selectMediaStream();
+/******/ })()
+;
